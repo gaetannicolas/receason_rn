a0b10894147e667327ced97e677813d86264afae
Gaetan Nicolas <gnicolas@appstud.com>
2020-03-23 09:24:06 +0100

    init firestore

--- src/modules/example/scenes/Example.js
+++ src/modules/example/scenes/Example.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import firestore from '@react-native-firebase/firestore'
 
-/**
- * Use functional components and hooks.
- */
 const Example = () => {
+  const [products, setProducts] = useState(null)
 
-  // Read the document for user 'Ada Lovelace':
-  // const documentSnapshot = firestore()
-  //   .collection('products')
-  //   .doc()
-  //   .get()
+  const getCollection = () => {
+    console.log('test')
+    return firestore().collection('products').onSnapshot((querySnapshot) => {
+      // Add users into an array
+      const productsList = querySnapshot.docs.map((documentSnapshot) => {
+        return {
+          ...documentSnapshot.data(),
+          key: documentSnapshot.id, // required for FlatList
+        }
+      })
 
-  // console.warn(documentSnapshot.data())
+      // Update state with the users array
+      setProducts(productsList)
+    })
+  }
+
+
+  useEffect(() => {
+    getCollection()
+  })
+
 
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.title}>Appstud React-native project starter</Text>
         <Text style={styles.text}>
-          Please (re)read the main README.md to learn how to organise your code!
+          Please (re)read the main README.md to  how to organise your code!
         </Text>
+        {products.map((item) => (
+          <Text>{item.name}</Text>
+        ))}
       </View>
     </View>
   )
